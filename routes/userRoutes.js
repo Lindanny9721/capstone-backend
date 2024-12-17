080753d2f951069db0279d52e9d65806968130fa
Lindanny9721 <32419597+Lindanny9721@users.noreply.github.com>
2024-12-17 04:35:37 -0500

    Added routes for login

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,27 +12,28 @@ router.post('/signup', async (req, res) => {
         const existingUsername = await User.findOne({ username });
         if (existingUsername) return res.status(400).json({ error: 'This username already exist.' });
         if (existingEmail) return res.status(400).json({ error: 'This email already exist.' });
-    const newUser = new User({ username, email, password });
-    await newUser.save();
-    res.status(200).json({ message: 'User created successfully' });
+        const newUser = new User({ username, email, password });
+        await newUser.save();
+        res.status(200).json({ message: 'User created successfully' });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Server error' });
     }
-  });  
-  router.post("/login", async (req, res) => {
+});  
+router.post("/login", async (req, res) => {
     const {email, password} = req.body;
     try {
         const user = await User.findOne({email});
+        if(!user) return res.status(404).json({ error: 'Invalid password or email' });
         user.comparePassword(password, (err,isMatch) => {
             if(err) return res.send("Server Error").status(500)
-            if(!isMatch) return res.status(404).send("Invalid password or email");
+            if(!isMatch) return res.status(404).json({ error: 'Invalid password or email' });
         })
         res.status(200).send("Login Successful");
-    } catch(e) {
+    } catch(error) {
         console.error(error);
-        res.status(404).send("Error logining");
+        res.status(404).json({ error: 'Error Login' });
     }
-})
+});
 
 export default router;
