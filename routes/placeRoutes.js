925ae3eb62258801b6c4c7f6c9a3c1bf5903cd9a
Lindanny9721 <32419597+Lindanny9721@users.noreply.github.com>
2024-12-17 08:31:16 -0500

    Added to route way to specify what places to search

--- routes/placeRoutes.js
+++ routes/placeRoutes.js
@@ -4,10 +4,15 @@ import axios from "axios";
 const router = express.Router();
 
 router.post('/places', async (req, res) => {
-    const { lat, lng, radius } = req.body;
+    const { lat, lng, radius, placeTypes } = req.body;
     try {
         const apiKey = process.env.GOOGLE_MAPS_API_KEY;
-        const placesUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${radius}&key=${apiKey}`;
+        let placesUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${radius}&key=${apiKey}`;
+        const selectedPlaceTypes = Object.keys(placeTypes).filter(type => placeTypes[type]);
+        if (selectedPlaceTypes.length > 0) {
+            placesUrl += `&type=${selectedPlaceTypes.join('|')}`;
+        }
+        console.log(placesUrl);
         const response = await axios.get(placesUrl);
         res.status(200).json(response.data.results);
     } catch (error) {
