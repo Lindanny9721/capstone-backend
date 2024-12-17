0b1dcb8566bf9b20175d6d54cbe1a13e8bda1fe0
Lindanny9721 <32419597+Lindanny9721@users.noreply.github.com>
2024-12-17 17:26:11 -0500

    Changed it so it only searches by one place at a time

--- routes/placeRoutes.js
+++ routes/placeRoutes.js
@@ -4,17 +4,25 @@ import axios from "axios";
 const router = express.Router();
 
 router.post('/places', async (req, res) => {
-    const { lat, lng, radius, placeTypes } = req.body;
+    const { lat, lng, radius, placeType } = req.body;
+    const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+    let placesUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${radius}&key=${apiKey}`;
+    if (placeType) {
+        placesUrl += `&type=${selectedPlaceTypes.join('|')}`;
+    }
+    console.log(placesUrl);
     try {
-        const apiKey = process.env.GOOGLE_MAPS_API_KEY;
-        let placesUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${radius}&key=${apiKey}`;
-        const selectedPlaceTypes = Object.keys(placeTypes).filter(type => placeTypes[type]);
-        if (selectedPlaceTypes.length > 0) {
-            placesUrl += `&type=${selectedPlaceTypes.join('|')}`;
-        }
-        console.log(placesUrl);
-        const response = await axios.get(placesUrl);
-        res.status(200).json(response.data.results);
+        let allPlaces = [];
+        let nextPageToken = null;
+        do {
+            const response = await axios.get(placesUrl + (nextPageToken ? `&pagetoken=${nextPageToken}` : ''));
+            allPlaces = [...allPlaces, ...response.data.results];
+            nextPageToken = response.data.next_page_token;
+            if (nextPageToken) {
+                await new Promise(resolve => setTimeout(resolve, 2000));
+            }
+        } while (nextPageToken);
+        res.status(200).json(allPlaces);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Error fetching places'});
